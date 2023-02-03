4bec2a63aaea8bb7f9f7d64030216e48125bda58
Jason Rivalino <jasonrivalino@gmail.com>
2023-02-03 15:37:48 +0700

    add program file

---
+++ src/main/java/Enums/PlayerActions.java
@@ -0,0 +1,19 @@
+package Enums;
+
+public enum PlayerActions {
+  FORWARD(1),
+  STOP(2),
+  START_AFTERBURNER(3),
+  STOP_AFTERBURNER(4),
+  FIRE_TORPEDOES(5),
+  FIRE_SUPERNOVA(6),
+  DETONATE_SUPERNOVA(7),
+  FIRE_TELEPORTER(8),
+  TELEPORTUSE_SHIELD(9);
+
+  public final Integer value;
+
+  private PlayerActions(Integer value) {
+    this.value = value;
+  }
+}
