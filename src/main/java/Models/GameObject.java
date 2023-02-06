5b0b795c2b9d756e26cedcad089c4d2a25bec004
archmans <13521010@std.stei.itb.ac.id>
2023-02-06 19:26:21 +0700

    Comment

--- src/main/java/Models/GameObject.java
+++ src/main/java/Models/GameObject.java
@@ -4,65 +4,65 @@ import Enums.*;
 import java.util.*;
 
 public class GameObject {
-  public UUID id;
-  public Integer size;
-  public Integer speed;
-  public Integer currentHeading;
-  public Position position;
-  public ObjectTypes gameObjectType;
+  public UUID id; // id dari objek
+  public Integer size; // ukuran dari objek
+  public Integer speed; // kecepatan dari objek
+  public Integer currentHeading; // merupakan sudut dari arah yang dituju
+  public Position position; // posisi dari objek
+  public ObjectTypes gameObjectType; // tipe dari objek
 
   public GameObject(UUID id, Integer size, Integer speed, Integer currentHeading, Position position, ObjectTypes gameObjectType) {
-    this.id = id;
-    this.size = size;
-    this.speed = speed;
-    this.currentHeading = currentHeading;
-    this.position = position;
-    this.gameObjectType = gameObjectType;
+    this.id = id; // id dari objek
+    this.size = size; // ukuran dari objek
+    this.speed = speed; // kecepatan dari objek
+    this.currentHeading = currentHeading; // merupakan sudut dari arah yang dituju
+    this.position = position; // posisi dari objek
+    this.gameObjectType = gameObjectType; // tipe dari objek
   }
 
-  public UUID getId() {
+  public UUID getId() { // getter id
     return id;
   }
 
-  public void setId(UUID id) {
+  public void setId(UUID id) { // setter id
     this.id = id;
   }
 
-  public int getSize() {
+  public int getSize() { // getter size
     return size;
   }
 
-  public void setSize(int size) {
+  public void setSize(int size) { // setter size
     this.size = size;
   }
 
-  public int getSpeed() {
+  public int getSpeed() { // getter speed
     return speed;
   }
 
-  public void setSpeed(int speed) {
+  public void setSpeed(int speed) { // setter speed
     this.speed = speed;
   }
 
-  public Position getPosition() {
+  public Position getPosition() { // getter position
     return position;
   }
 
-  public void setPosition(Position position) {
+  public void setPosition(Position position) { // setter position
     this.position = position;
   }
 
-  public ObjectTypes getGameObjectType() {
+  public ObjectTypes getGameObjectType() { // getter gameObjectType
     return gameObjectType;
   }
 
-  public void setGameObjectType(ObjectTypes gameObjectType) {
+  public void setGameObjectType(ObjectTypes gameObjectType) { // setter gameObjectType
     this.gameObjectType = gameObjectType;
   }
 
   public static GameObject FromStateList(UUID id, List<Integer> stateList)
   {
-    Position position = new Position(stateList.get(4), stateList.get(5));
-    return new GameObject(id, stateList.get(0), stateList.get(1), stateList.get(2), position, ObjectTypes.valueOf(stateList.get(3)));
+    Position position = new Position(stateList.get(4), stateList.get(5)); // posisi dari objek
+    return new GameObject(id, stateList.get(0), stateList.get(1), stateList.get(2), position, ObjectTypes.valueOf(stateList.get(3))); 
   }
 }
