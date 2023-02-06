e60f298041d1db12d73084c53c24336ab581f7f5
archmans <13521010@std.stei.itb.ac.id>
2023-02-06 19:26:52 +0700

    Add comment

--- src/main/java/Models/GameStateDto.java
+++ src/main/java/Models/GameStateDto.java
@@ -2,33 +2,33 @@ package Models;
 
 import java.util.*;
 
-public class GameStateDto {
+public class GameStateDto { // kelas untuk menyimpan state dari game
 
-  private World world;
-  private Map<String, List<Integer>> gameObjects;
-  private Map<String, List<Integer>> playerObjects;
+  private World world; // menyimpan state dari world
+  private Map<String, List<Integer>> gameObjects; // menyimpan state dari game object
+  private Map<String, List<Integer>> playerObjects; // menyimpan state dari player game object
 
-  public Models.World getWorld() {
+  public Models.World getWorld() { // getter world
     return world;
   }
 
-  public void setWorld(Models.World world) {
+  public void setWorld(Models.World world) { // setter world
     this.world = world;
   }
 
-  public Map<String, List<Integer>> getGameObjects() {
+  public Map<String, List<Integer>> getGameObjects() { // getter game object
     return gameObjects;
   }
 
-  public void setGameObjects(Map<String, List<Integer>> gameObjects) {
+  public void setGameObjects(Map<String, List<Integer>> gameObjects) { // setter game object
     this.gameObjects = gameObjects;
   }
 
-  public Map<String, List<Integer>> getPlayerObjects() {
+  public Map<String, List<Integer>> getPlayerObjects() { // getter player game object
     return playerObjects;
   }
 
-  public void setPlayerObjects(Map<String, List<Integer>> playerObjects) {
-    playerObjects = playerObjects;
+  public void setPlayerObjects(Map<String, List<Integer>> playerObjects) { // setter player game object
+    this.playerObjects = playerObjects;
   }
 }
