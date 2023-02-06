e4b94ddc64c1d4adb4d8e3b5536f10535e55271b
archmans <13521010@std.stei.itb.ac.id>
2023-02-06 19:26:38 +0700

    Comment

--- src/main/java/Models/GameState.java
+++ src/main/java/Models/GameState.java
@@ -2,45 +2,45 @@ package Models;
 
 import java.util.*;
 
-public class GameState {
+public class GameState { // kelas untuk menyimpan state dari game
 
-    public World world;
-    public List<GameObject> gameObjects;
-    public List<GameObject> playerGameObjects;
+    public World world; // menyimpan state dari world
+    public List<GameObject> gameObjects; // menyimpan state dari game object
+    public List<GameObject> playerGameObjects; // menyimpan state dari player game object
 
-    public GameState() {
-        world = new World();
-        gameObjects = new ArrayList<GameObject>();
-        playerGameObjects = new ArrayList<GameObject>();
+    public GameState() { // constructor
+        world = new World(); // inisialisasi world
+        gameObjects = new ArrayList<GameObject>(); // inisialisasi game object
+        playerGameObjects = new ArrayList<GameObject>(); // inisialisasi player game object
     }
 
-    public GameState(World world , List<GameObject> gameObjects, List<GameObject> playerGameObjects) {
+    public GameState(World world , List<GameObject> gameObjects, List<GameObject> playerGameObjects) { // constructor
         this.world = world;
         this.gameObjects = gameObjects;
         this.playerGameObjects = playerGameObjects;
     }
 
-    public World getWorld() {
+    public World getWorld() { // getter world
         return world;
     }
 
-    public void setWorld(World world) {
+    public void setWorld(World world) { // setter world
         this.world = world;
     }
 
-    public List<GameObject> getGameObjects() {
+    public List<GameObject> getGameObjects() { // getter game object
         return gameObjects;
     }
 
-    public void setGameObjects(List<GameObject> gameObjects) {
+    public void setGameObjects(List<GameObject> gameObjects) { // setter game object
         this.gameObjects = gameObjects;
     }
 
-    public List<GameObject> getPlayerGameObjects() {
+    public List<GameObject> getPlayerGameObjects() { // getter player game object
         return playerGameObjects;
     }
 
-    public void setPlayerGameObjects(List<GameObject> playerGameObjects) {
+    public void setPlayerGameObjects(List<GameObject> playerGameObjects) { // setter player game object
         this.playerGameObjects = playerGameObjects;
     }
 
