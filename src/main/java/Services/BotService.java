732dbdf37902dc319ca7f8c1c3a4b9e76e47f38e
archmans <13521010@std.stei.itb.ac.id>
2023-02-11 17:00:36 +0700

    Rapihin code

--- src/main/java/Services/BotService.java
+++ src/main/java/Services/BotService.java
@@ -41,49 +41,23 @@ public class BotService {
     }
 
     public void computeNextPlayerAction(PlayerAction playerAction) {
-        // greedy algorithm entelect challenge galaxio 2021
-        // player action move forward
         playerAction.action = PlayerActions.FORWARD;
-        // move to list food : FOOD, SUPERFOOD
         if (!gameState.getGameObjects().isEmpty()) {
-            // Greedy by maximalize size of bot
-            // get list food
             var food = gameState.getGameObjects().stream().filter(gameObject -> gameObject.getGameObjectType() == ObjectTypes.SUPERFOOD).collect(Collectors.toList());
             var foodd = gameState.getGameObjects().stream().filter(gameObject -> gameObject.getGameObjectType() == ObjectTypes.FOOD).collect(Collectors.toList());
-            // get nearest food
-            var nearestFood = food.stream().min(Comparator.comparing(gameObject -> getDistanceBetween(gameObject, bot))).get();
-            // System.out.println("Super Food Berhasil");
-            var nearestFoodd = foodd.stream().min(Comparator.comparing(gameObject -> getDistanceBetween(gameObject, bot))).get();
-            // System.out.println("Food Berhasil");
-            // get nearest obstacle
             var obstacle = gameState.getGameObjects().stream().filter(gameObject -> gameObject.getGameObjectType() == ObjectTypes.GASCLOUD || gameObject.getGameObjectType() == ObjectTypes.ASTEROIDFIELD).collect(Collectors.toList());
-            var nearestObstacle = obstacle.stream().min(Comparator.comparing(gameObject -> getDistanceBetween(gameObject, bot))).get();
-            // System.out.println("Obstacle Berhasil");
-            // get enemy
-            // get nearest enemy
-            
             var enemy = gameState.getPlayerGameObjects().stream().filter(gameObject -> gameObject.getGameObjectType() == ObjectTypes.PLAYER && gameObject.getId() != bot.getId()).collect(Collectors.toList());
+            var nearestFood = food.stream().min(Comparator.comparing(gameObject -> getDistanceBetween(gameObject, bot))).get();
+            var nearestFoodd = foodd.stream().min(Comparator.comparing(gameObject -> getDistanceBetween(gameObject, bot))).get();
+            var nearestObstacle = obstacle.stream().min(Comparator.comparing(gameObject -> getDistanceBetween(gameObject, bot))).get();
             var nearestEnemy = enemy.stream().min(Comparator.comparing(gameObject -> getDistanceBetween(gameObject, bot))).get();
             var smallestEnemy = enemy.stream().min(Comparator.comparing(gameObject -> gameObject.getSize()-bot.getSize())).get();
-            // System.out.println("Enemy Berhasil");
-            
-            
-            
-            // get real distance between player and enemy
-            // var distanceEnemy = getDistanceBetween(nearestEnemy, bot) - nearestEnemy.getSize()/2 - bot.getSize()/2;
-            
             // var torpedo = gameState.getGameObjects().stream().filter(gameObject -> gameObject.getGameObjectType() == ObjectTypes.TORPEDOSALVO).collect(Collectors.toList());
             // var nearestTorpedo = torpedo.stream().min(Comparator.comparing(gameObject -> getDistanceBetween(gameObject, bot))).get();
-            // System.out.println("Torpedo Berhasil");
-            
-            // System.out.println("Torpedo Gagal");
-            
-            
-
 
             var radiusWorld = gameState.getWorld().getRadius();
             // eat SUPERFOOD and FOOD and avoid obstacle and enemy
-            if(true){
+            if(bot.getSize() < 100){
                 if(isMakanSuper) {
                     var headingToFoodd = getHeadingBetween(nearestFoodd);
                     playerAction.heading = headingToFoodd;
@@ -116,14 +90,6 @@ public class BotService {
                         isMakanSuper = true;
                         tik = gameState.getWorld().getCurrentTick();
                     }
-                    // if(bot.getSize() < 50){
-                    // }else if(bot.getSize() < 100){
-                    //     if(getRealDistance(bot, nearestFood) < ) {
-                    //         System.out.println("SUPER FOOD");
-                    //         isMakanSuper = true;
-                    //         tik = gameState.getWorld().getCurrentTick();
-                    //     }
-                    // }
                 }
                 // && gameState.getWorld().getCurrentTick() > 100
                 if(bot.size>50 && teleportAvailable ) {
@@ -137,58 +103,14 @@ public class BotService {
                 }
             }
             else{
-                // if (getRealDistance(bot, nearestEnemy) < 250 && bot.getSize() > 25) {
-                //         // set heading to enemy
-                //         System.out.println("FIRE FIRE");
-                //         var headingToEnemy = getHeadingBetween(nearestEnemy);
-                //         playerAction.setHeading(headingToEnemy);
-                //         playerAction.action = PlayerActions.FIRETORPEDOES;
-                //     }
+                if (getRealDistance(bot, nearestEnemy) < 250 && bot.getSize() > 25) {
+                        // set heading to enemy
+                        System.out.println("FIRE FIRE");
+                        var headingToEnemy = getHeadingBetween(nearestEnemy);
+                        playerAction.setHeading(headingToEnemy);
+                        playerAction.action = PlayerActions.FIRETORPEDOES;
+                    }
             }
-            
-
-            
-
-            // playerAction.heading = getHeadingBetween(nearestObstacle);
-
-            
-
-        //     // greedy by attack enemy use FIRETORPEDOES
-        //     // attack enemy if distance between bot and enemy is less than 250
-            // if (getRealDistance(bot, nearestEnemy) < 250 && bot.getSize() > 25) {
-            //     // set heading to enemy
-            //     System.out.println("FIRE FIRE");
-            //     var headingToEnemy = getHeadingBetween(nearestEnemy);
-            //     playerAction.setHeading(headingToEnemy);
-            //     playerAction.action = PlayerActions.FIRETORPEDOES;
-            //     this.playerAction = playerAction;
-            //     // teleport to enemy if size of enemy is less than size of bot
-            //     // if the enemy is smaller than the bot, fire teleporter
-            // }
-
-            
-            // System.out.println("ENEMY 1 :");
-            // System.out.println("SIZE\t:" + enemy.get(0).getSize());
-            // System.out.println("SALVO\t:" + enemy.get(0).getTorpedoSalvoCount());
-            // System.out.println("ENEMY 2 :");
-            // System.out.println("SIZE\t:" + enemy.get(1).getSize());
-            // System.out.println("SALVO\t:" + enemy.get(1).getTorpedoSalvoCount());
-            // System.out.println("BOT :");
-            // System.out.println("SIZE\t:" + bot.getSize());
-            // System.out.println("SALVO\t:" + bot.getTorpedoSalvoCount());
-
-            // if(enemy.get(0).torpedoSalvoCount == 0 || enemy.get(1).torpedoSalvoCount == 0){
-            //     System.out.println("SHILEDDDDDDDD");
-            //     playerAction.action = PlayerActions.ACTIVATESHIELD;
-            // }
-
-            // if (getRealDistance(nearestEnemy, bot) < 400 &&  nearestEnemy.getSize() < bot.getSize()-25) {
-            //     playerAction.action = PlayerActions.FIRETELEPORTER;
-            //     var teleporter = gameState.getGameObjects().stream().filter(gameObject -> gameObject.getGameObjectType() == ObjectTypes.TELEPORTER).collect(Collectors.toList());
-            //     if (getRealDistance(teleporter.get(0), enemy.get(0)) <= 30) {
-            //         playerAction.action = PlayerActions.TELEPORT;
-            //     }
-            // }
         }
         this.playerAction = playerAction;
     } 
@@ -232,40 +154,3 @@ public class BotService {
 
 
 }
-
-// playerAction.action = PlayerActions.FORWARD;
-// int heading = playerAction.getHeading();
-// // playerAction.heading = new Random().nextInt(360);
-// if (!gameState.getGameObjects().isEmpty()) {
-//     var superFoodList = gameState.getGameObjects()
-//     .stream().filter(item -> item.getGameObjectType() == ObjectTypes.SUPERFOOD)
-//     .sorted(Comparator
-//     .comparing(item -> getDistanceBetween(bot, item)))
-//     .collect(Collectors.toList());
-
-//     var obstacleList = gameState.getGameObjects()
-//     .stream().filter(musuh -> musuh.getGameObjectType() == ObjectTypes.GASCLOUD ||
-//     musuh.getGameObjectType() == ObjectTypes.ASTEROIDFIELD ||
-//     musuh.getGameObjectType() == ObjectTypes.TELEPORTER)
-//     .sorted(Comparator
-//     .comparing(musuh -> getDistanceBetween(bot, musuh)))
-//     .collect(Collectors.toList());
-
-//     var enemyList = gameState.getGameObjects()
-//     .stream().filter(enemy -> (enemy.getId() != bot.getId()) && (enemy.getGameObjectType() == ObjectTypes.PLAYER))
-//     .sorted(Comparator
-//     .comparing(enemy -> getDistanceBetween(bot, enemy)))
-//     .collect(Collectors.toList());
-    
-//     // if(getDistanceBetween(bot, superFoodList.get(0)) <= 300){
-//         // playerAction.heading = getHeadingBetween(superFoodList.get(0));
-//         if(getDistanceBetween(bot, enemyList.get(0)) < 600){
-//             int musuh = enemyList.get(0).getSize();
-//             int aku = bot.getSize();
-//             // if(musuh<aku){
-//                 playerAction.heading = getHeadingBetween(enemyList.get(0));
-//             // }
-//         }
-//     // }
-
-// }
